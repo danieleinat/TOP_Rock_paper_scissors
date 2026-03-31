a13cf828f351c289400eb0e9740711a07a5f6b4c
Daniel Einat <191679047+danieleinat@users.noreply.github.com>
2026-03-31 07:28:15 +0300

    message

--- script4.js
+++ script4.js
@@ -1,38 +1,34 @@
-function playRound(computerChoice, humanChoice) 
+function playRound(humanChoice,computerChoice)
 {
- let x= 'rock';
- let y = 'paper';
-  let z = 'scissors';
-  x > z;
-  z > y;
-  y > x;
-  computerChoice = 0;
-  humanChoice = 0;
-  if (computerChoice>humanChoice){
-    computerChoice++
-  console.log('You lose!')
-  } else if (computerChoice < humanChoice){
-    humanChoice++;
-    console.log('You win! ')
-  }else {console.log("it'ts a tie")
-  }}
-  
-
+console.log(`computer has , ${computerChoice}`)
+console.log(`you  have , ${humanChoice}`);
+humanChoice = 0;
+computerChoice = 0;
+ if (humanSelection = 'rock' && computerSelection == scissors || humanSelection == 'paper' && computerSelection ==  'rock' || humanSelection == 'scissors' && computerSelection == 'paper'){ console.log("YOU WIN")
+ 
+ humanChoice++;
+ };
+ else if (computerSelection = 'rock' && humanSelection == scissors || computerSelection == 'paper' && humanSelection ==  'rock' || computerSelection == 'scissors' && humanSelection == 'paper') console.log("YOU lose"){
+ computerChoice++};
+ else
+  {
+  console.log('it's a tie)
+ };
+}
 // humen's input
 function getHumanChoice() {
   humanChoice = prompt("enter your answer: ");
   humanChoice = humanChoice.toLowerCase();
-  console.log(`you  have , ${humanChoice}`);
   return humanChoice;
 }
 
 // computer input
 function getComputerChoice() {
   var computerChoice = Math.random()
-  if (computerChoice < 0.25) computerChoice = "rock";
-  else if (0.5 < computerChoice < 0.75)computerChoice = "paper";
-  else computerChoice = "scissors";
-  console.log(`computer has , ${computerChoice}`)
+  if (computerChoice < 0.25)  return "rock";
+  else if (0.5 < computerChoice && computerChoice < 0.75)return  "paper";
+  else return "scissors";
+
 
 
 }
@@ -41,4 +37,3 @@ const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection);
-playRound(computerChoice, humanChoice) 
