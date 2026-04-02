5c25c8d4dcac236cdd13551875a6d2cd3f537b9d
Daniel Einat <191679047+danieleinat@users.noreply.github.com>
2026-04-02 21:13:33 +0300

    rendition of the code from scratch playgame*

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-function main(num){
-    if (num < 0.25)
-    console.log('rock')
-    else if ( 0.5 < num < 0.75)
-    console.log('paper')
-    else
-        console.log('scissors')
-    
+// Create a new function named getComputerChoice.
+// Create two new variables named humanScore and computerScore in the global scope.
+// Initialize those variables with the value of 0.
+let humanScore = 0;
+let computerScore = 0;
+
+// Create a new function named playRound.
+// Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices as arguments.
+
+function playRound(humanChoice, computerChoice) {
+  console.log(`computer has - ${computerChoice}`);
+  console.log(`you  have - ${humanChoice}`);
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    // Increment the humanScore or computerScore variable based on the round winner.
+    humanChoice++;
+    console.log("YOU WIN");
+  } else if (
+    (computerChoice === "rock" && humanChoice === "scissors") ||
+    (computerChoice === "paper" && humanChoice === "rock") ||
+    (computerChoice === "scissors" && humanChoice === "paper")
+  ) {
+    // Increment the humanScore or computerScore variable based on the round winner.
+    computerChoice++;
+    console.log("YOU LOSE");
+  } else {
+    console.log("it's a tie");
+  }
 }
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
-
-function getComputerChoice(){
-    return(Math.random());
-    
-}
-
-let num = getComputerChoice()
-main(num)
+// Create a new function named playGame.
+// Move your playRound function and score variables so that they’re declared inside of the new playGame function
+// Play 5 rounds by calling playRound 5 times.
